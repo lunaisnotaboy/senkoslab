611e32ff1c33105cf28d7f18198682620ff6c661
Robert Wilcox <stampylongr7@gmail.com>
2020-05-13 20:56:01 -0400

    did stuff

---
+++ main.js
@@ -0,0 +1,9 @@
+const snoowrap = require('snoowrap');
+
+const otherRequester = new snoowrap({
+    userAgent: 'put your user-agent string here',
+    clientId: 'put your client id here',
+    clientSecret: 'put your client secret here',
+    username: 'put your username here',
+    password: 'put your password here'
+});
